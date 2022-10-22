109aa864428920c90ccc26344c3487672333a0b6
Mitch Henline <rmhenline@gmail.com>
2022-10-22 01:02:25 -0600

    next step is delete previous plant when click bttn

--- client/main.js
+++ client/main.js
@@ -18,6 +18,7 @@ function createPlantCard(plant){
 
     plantCard.innerHTML = `<img alt="plant picture" src=${plant.imageURL} class="plant-picture"/>
     <p class ="plant-title">${plant.title}</p>
+    <p class ="scientific-name">${plant.sName}</p>
     <p class ="plant-type">Type: ${plant.type}</p>
     </div>`
 
